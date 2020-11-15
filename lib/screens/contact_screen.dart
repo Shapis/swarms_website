17ad29e15d7aa98786a0027085187b864e81e98f
Henrique Silva <hpsilva+git@pm.me>
2020-11-15 12:32:42 -0300

    WIP: Fixing validators in the contact screen

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -1,110 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:minimal/screens/base_screen.dart';
 
-class ContactScreen extends StatelessWidget {
+class ContactScreen extends StatefulWidget {
   static const String id = 'contact_screen';
 
-  final Widget sizedBox = SizedBox(height: 30);
+  @override
+  _ContactScreenState createState() => _ContactScreenState();
+}
 
+class _ContactScreenState extends State<ContactScreen> {
+  final Widget sizedBox = SizedBox(height: 30);
   final _UsNumberTextInputFormatter _phoneNumberFormatter =
       _UsNumberTextInputFormatter();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _name;
+  String _email;
+  String _phoneNumber;
+  String _textBody;
 
   @override
   Widget build(BuildContext context) {
     return BaseScreen(
       child: [
-        TextFormField(
-          decoration: InputDecoration(
-            filled: true,
-            icon: const Icon(Icons.person),
-            hintText: 'What do people call you?',
-            labelText: 'Name*',
+        Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: InputDecoration(
+                  filled: true,
+                  icon: const Icon(Icons.person),
+                  hintText: 'What do people call you?',
+                  labelText: 'Name*',
+                ),
+                keyboardType: TextInputType.name,
+                onChanged: (value) {
+                  _name = value;
+                },
+                validator:
+                    RequiredValidator(errorText: 'this field is required'),
+              ),
+              sizedBox,
+              TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                decoration: InputDecoration(
+                  filled: true,
+                  icon: const Icon(Icons.email),
+                  hintText: 'Your email address',
+                  labelText: 'Email*',
+                ),
+                validator: MultiValidator(
+                    [EmailValidator(errorText: 'enter a valid email address')]),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  _email = value;
+                },
+                //validator: validators.isEmail,
+              ),
+              sizedBox,
+              IntlPhoneField(
+                //autoValidate: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  hintText: 'Where can I reach you?',
+                  labelText: 'Phone number',
+                ),
+                onChanged: (value) {
+                  _phoneNumber = value.completeNumber;
+                },
+              ),
+              sizedBox,
+              TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                validator:
+                    RequiredValidator(errorText: 'this field is required'),
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  hintText:
+                      'Tell us how Tyler can help you (e.g. write down what services you require)',
+                  helperText: 'We\'ll get back to you as soon as possible',
+                  labelText: 'Talk to us',
+                ),
+                maxLines: 3,
+                onChanged: (value) {
+                  _textBody = value;
+                },
+              ),
+              sizedBox,
+              Center(
+                child: RaisedButton(
+                  child: Text('SUBMIT'),
+                  onPressed: () {
+                    print(_formKey.currentState.validate());
+                  },
+                ),
+              ),
+              sizedBox,
+              Text(
+                '* indicates required field',
+                style: Theme.of(context).textTheme.caption,
+              ),
+              sizedBox,
+            ],
           ),
-          keyboardType: TextInputType.name,
-          onChanged: (value) {
-            print(value);
-          },
-          //maxLength: 14,
-          maxLengthEnforced: false,
-          // validator: _validatePhoneNumber,
-          // TextInputFormatters are applied in sequence.
-          // inputFormatters: <TextInputFormatter>[
-          //   FilteringTextInputFormatter.digitsOnly,
-          //   // Fit the validating format.
-          //   _phoneNumberFormatter,
-          // ],
         ),
-        sizedBox,
-        TextFormField(
-          decoration: InputDecoration(
-            filled: true,
-            icon: const Icon(Icons.phone),
-            hintText: 'Where can we reach you?',
-            labelText: 'Phone number',
-            prefixText: '+1 ',
-          ),
-          keyboardType: TextInputType.phone,
-          onSaved: (value) {
-            print(value);
-          },
-          maxLength: 14,
-          maxLengthEnforced: false,
-
-          // validator: _validatePhoneNumber,
-          // TextInputFormatters are applied in sequence.
-          inputFormatters: <TextInputFormatter>[
-            FilteringTextInputFormatter.digitsOnly,
-            // Fit the validating format.
-            _phoneNumberFormatter,
-          ],
-        ),
-        sizedBox,
-        TextFormField(
-          decoration: InputDecoration(
-            filled: true,
-            icon: const Icon(Icons.email),
-            hintText: 'Your email address',
-            labelText: 'Email*',
-          ),
-          keyboardType: TextInputType.emailAddress,
-          onSaved: (value) {
-            print(value);
-          },
-          //maxLength: 14,
-          maxLengthEnforced: false,
-          // validator: _validatePhoneNumber,
-          // TextInputFormatters are applied in sequence.
-          // inputFormatters: <TextInputFormatter>[
-          //   FilteringTextInputFormatter.digitsOnly,
-          //   // Fit the validating format.
-          //   _phoneNumberFormatter,
-          // ],
-        ),
-        sizedBox,
-        TextFormField(
-          decoration: InputDecoration(
-            border: const OutlineInputBorder(),
-            hintText:
-                'Tell us how Tyler can help you (e.g. write down what services you require)',
-            helperText: 'We\'ll get back to you as soon as possible',
-            labelText: 'Talk to us',
-          ),
-          maxLines: 3,
-        ),
-        sizedBox,
-        Center(
-          child: RaisedButton(
-            child: Text('SUBMIT'),
-            onPressed: () {},
-          ),
-        ),
-        sizedBox,
-        Text(
-          '* indicates required field',
-          style: Theme.of(context).textTheme.caption,
-        ),
-        sizedBox,
       ],
     );
   }
